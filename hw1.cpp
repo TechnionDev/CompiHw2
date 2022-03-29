b8237dd05a1da3961e11551ca912b7f7f787ab38
amitir22 <amitir22@gmail.com>
2022-03-29 19:28:39 +0300

    started to implement hw1.cpp added some todo-s consider switching from snake_case to camelCase

--- hw1.cpp
+++ hw1.cpp
@@ -1,10 +1,52 @@
+#include <string>
+#include <iostream>
 #include "tokens.hpp"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+void print_invalid_token();
+void print_token(int token);
+void print_comment_token();
+void print_string_token();
 
 int main()
 {
 	int token;
+
 	while ((token = yylex())) {
-	  // Your code here
+        if (token == -1) {
+            print_invalid_token();
+        } else {
+            print_token(token);
+        }
 	}
+
 	return 0;
+}
+
+void print_invalid_token() {
+    // TODO: implement
+    // TODO: consider pushing an "exit(-1)" after print if necessary
+}
+
+void print_token(int token) {
+    cout << yylineno << " " << token << " ";
+
+    if (token == COMMENT) {
+        print_comment_token();
+    } else if (token == STRING) {
+        print_string_token();
+    } else {
+        cout << yytext << endl;
+    }
+}
+
+void print_comment_token() {
+    cout << "//" << endl;
+}
+
+void print_string_token() {
+    // TODO: implement
 }
